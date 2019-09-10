2f494ce126827a0ac0b7ad5ceac2d29fd63c560f
andreaaguilar3a <wanderlust3a@gmail.com>
2019-09-10 13:33:21 -0300

    van img y subir main

--- src/main.js
+++ src/main.js
@@ -1,1 +1,75 @@
-/* Manejo del DOM */
+const data1 = window.WORLDBANK;
+
+let peru = Object.values(data1.PER.indicators);
+
+const peruButton = document.getElementById("buttonPer");
+peruButton.addEventListener("click", dataPeru);
+
+function dataPeru() {
+    let initialDiv1 = document.getElementById("root").innerHTML = "";
+
+    for (let i = 0; i < peru.length; i++) {
+        let container = document.createElement("div");
+        let nameIndicatorPer = document.createElement("h4");
+
+        nameIndicatorPer.textContent = peru[i].indicatorName;
+        container.appendChild(nameIndicatorPer);
+        document.getElementById("root").appendChild(container).innerHTML;
+    }
+}
+
+let mexico = Object.values(data1.MEX.indicators);
+
+const mexicoButton = document.getElementById("buttonMex");
+mexicoButton.addEventListener("click", dataMexico);
+
+function dataMexico() {
+    let initialDiv2 = document.getElementById("root").innerHTML = "";
+
+    for (let i = 0; i < mexico.length; i++) {
+        let container = document.createElement("div");
+        let nameIndicatorMex = document.createElement("h4");
+
+        nameIndicatorMex.textContent = mexico[i].indicatorName;
+        container.appendChild(nameIndicatorMex);
+        document.getElementById("root").appendChild(container).innerHTML;
+
+    }
+}
+
+let brasil = Object.values(data1.BRA.indicators);
+
+const brasilButton = document.getElementById("buttonBra");
+brasilButton.addEventListener("click", dataBrasil);
+
+function dataBrasil() {
+    let initialDiv3 = document.getElementById("root").innerHTML = "";
+
+    for (let i = 0; i < brasil.length; i++) {
+        let container = document.createElement("div");
+        let nameIndicatorBra = document.createElement("h4");
+
+        nameIndicatorBra.textContent = brasil[i].indicatorName;
+        container.appendChild(nameIndicatorBra);
+        document.getElementById("root").appendChild(container).innerHTML;
+
+    }
+}
+
+let chile = Object.values(data1.CHL.indicators);
+
+const chileButton = document.getElementById("buttonChl");
+chileButton.addEventListener("click", dataChile);
+
+function dataChile() {
+    let initialDiv4 = document.getElementById("root").innerHTML = "";
+
+    for (let i = 0; i < chile.length; i++) {
+        let container = document.createElement("div");
+        let nameIndicatorChl = document.createElement("h4");
+
+        nameIndicatorChl.textContent = chile[i].indicatorName;
+        container.appendChild(nameIndicatorChl);
+        document.getElementById("root").appendChild(container).innerHTML;
+    }
+}
